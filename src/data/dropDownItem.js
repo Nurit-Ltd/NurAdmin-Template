54e2641361f88e516b0a74abf997d48f5ce1f576
SymuNadeem <msymunnadeem@gmail.com>
2024-03-31 16:31:46 +0600

    all ui components with nurAdmin

--- src/data/dropDownItem.js
+++ src/data/dropDownItem.js
@@ -1,16 +1,16 @@
-import HtmlSvg from "../components/common/HtmlSvg";
+// import HtmlSvg from "../components/common/HtmlSvg";
 import NextSvg from "../components/common/NextSvg";
 import ReactSvg from "../components/common/ReactSvg";
-import VueSvg from "../components/common/VueSvg";
+// import VueSvg from "../components/common/VueSvg";
 
 
 export const dropDownItem = [
-  {
-    id: 1,
-    icon: HtmlSvg ,
-    text: "Preview HTML",
-    link: "https://nur-admin-dashboard.vercel.app/"
-  },
+  // {
+  //   id: 1,
+  //   icon: HtmlSvg ,
+  //   text: "Preview HTML",
+  //   link: "https://nur-admin-dashboard.vercel.app/"
+  // },
   {
     id: 2,
     icon: ReactSvg ,
@@ -23,10 +23,10 @@ export const dropDownItem = [
     text: "Preview Next",
     link: "https://nur-admin-dashboard.vercel.app/"
   },
-  {
-    id: 4,
-    icon: VueSvg ,
-    text: "Preview Vue",
-    link: "https://nur-admin-dashboard.vercel.app/"
-  }
+  // {
+  //   id: 4,
+  //   icon: VueSvg ,
+  //   text: "Preview Vue",
+  //   link: "https://nur-admin-dashboard.vercel.app/"
+  // }
 ];
